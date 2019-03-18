b2ae1473c8b4be56212655c485904ea26d31ef3e
szymon-szym <35077381+szymon-szym@users.noreply.github.com>
2019-03-18 22:24:05 +0100

    playing around
    fonts and dark scheme

---
+++ components/CardMain.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="bg-deep border border-solid border-white h-24 transparent"> {{ text }} </div>
+</template>
+
+<script>
+export default {
+  name: 'CardMain',
+  data() {
+    return {
+      text: 'lorem ipsum'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <header class="p-3 h-10 w-full absolute block bg-primary text-white flex items-center justify-between">
-      <h1>Nuxt + Tailwind CSS</h1>
-      <a href="/contact">contact</a>
+    <header class="p-3 h-10 w-full absolute block bg-deep text-white flex items-center justify-between">
+      <h1 class="font-serif">CRL</h1>
+      <nuxt-link class="text-white text-sm font-serif" to="/"> HOME </nuxt-link>
     </header>
-    <div class="h-screen bg-grey flex items-center justify-around">
+    <div class="h-screen p-12 bg-deep flex items-start justify-around">
       <nuxt/>
     </div>
-    <footer class="absolute p-3 h-10 w-full bg-black text-white flex items-center justify-between pin-b">
-      <p><small>Ben</small></p>
-      <p><small>&copy; copyright</small></p>
+    <footer class="absolute p-3 h-10 w-full bg-deep text-white flex items-center justify-between pin-b">
+      <p><small>2019</small></p>
+      <p><small></small></p>
     </footer>
   </div>
 </template>
@@ -20,7 +20,7 @@ h1 {
 }
 
 a {
-  @apply text-link no-underline
+  @apply  no-underline
 }
 
 a:hover {
